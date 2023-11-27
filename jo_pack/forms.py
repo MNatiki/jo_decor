c4c492fe05bb6f47bc70a3af078b9aebf5e8f6ad
Nati Niguse <natiart07@gmail.com>
2023-11-27 11:15:25 +0300

    jo decor

--- jo_pack/forms.py
+++ jo_pack/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from jo_pack.models import User
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -33,12 +35,33 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class OrderForm(FlaskForm):
-    id = IntegerField('Phone')
+    id = IntegerField('Row ID')
     username = StringField('Full Name', validators=[DataRequired(), Length(min=2, max=20)])
     phone = IntegerField('Phone', validators=[DataRequired()])
     address = StringField('Address')
     choice = TextAreaField('What is your choice', validators=[DataRequired()])
     submit = SubmitField('Order')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
 
 
 
--- jo_pack/routes.py
+++ jo_pack/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template, url_for, flash, redirect, request, get_flashed_messages
-from jo_pack.forms import RegistrationForm, LoginForm, OrderForm
+from jo_pack.forms import RegistrationForm, LoginForm, OrderForm, UpdateAccountForm
 from jo_pack.models import User, Orders
 from flask_bcrypt import Bcrypt
+from PIL import Image
+import secrets
+import os
 from jo_pack import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -36,6 +39,7 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
+            flash('Your logged in successfully!', 'success')
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
@@ -108,3 +112,37 @@ def done():
         flash('Form validation failed', 'danger')
 
     return render_template('notify.html', title='Done', form=form)
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
